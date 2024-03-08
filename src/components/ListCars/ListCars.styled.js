67a164732392628ee38fdb4e4b91b7069e8def12
Oksana Marusich <oksana.m229@gmail.com>
2024-03-08 23:03:40 +0200

    change style listCard

--- src/components/ListCars/ListCars.styled.js
+++ src/components/ListCars/ListCars.styled.js
@@ -4,9 +4,6 @@ export const ContainerListCars = styled.ul`
   owerflow: hidden;
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
-  row-gap: 50px;
-  column-gap: 29px;
+  gap: 50px 29px;
   margin-bottom: 100px;
-  
 `;
